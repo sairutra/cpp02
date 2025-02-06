8c8b4f6db9188a2179ed93ae682997f56d983631
spenning <whimizera@mailbox.org>
2025-02-06 18:54:01 +0100

    changed main

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -3,33 +3,13 @@
 int main(void)
 {
 	Fixed a;
-	Fixed const b( 10 );
-	Fixed const c(-167772);
-	Fixed const d( b );
-	Fixed const e( -1000 );
-	Fixed const f( -1000.1421784f );
+	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+	std::cout << a << std::endl;
+	std::cout << ++a << std::endl;
+	std::cout << a << std::endl;
+	std::cout << a++ << std::endl;
+	std::cout << a << std::endl;
+	std::cout << b << std::endl;
 
-	a = Fixed( 1234.4321f );
-
-	std::cout << "a is " << a << std::endl;
-	std::cout << "b is " << b << std::endl;
-	std::cout << "c is " << c << std::endl;
-	std::cout << "d is " << d << std::endl;
-	std::cout << "e is " << e << std::endl;
-	std::cout << "f is " << f << std::endl;
-
-	std::cout << "a is " << a.toInt() << " as integer" << std::endl;
-	std::cout << "b is " << b.toInt() << " as integer" << std::endl;
-	std::cout << "c is " << c.toInt() << " as integer" << std::endl;
-	std::cout << "d is " << d.toInt() << " as integer" << std::endl;
-	std::cout << "e is " << e.toInt() << " as integer" << std::endl;
-	std::cout << "f is " << f.toInt() << " as integer" << std::endl;
-
-	std::cout << "a is " << a.toFloat() << " as float" << std::endl;
-	std::cout << "b is " << b.toFloat() << " as float" << std::endl;
-	std::cout << "c is " << c.toFloat() << " as float" << std::endl;
-	std::cout << "d is " << d.toFloat() << " as float" << std::endl;
-	std::cout << "e is " << e.toFloat() << " as float" << std::endl;
-	std::cout << "f is " << f.toFloat() << " as float" << std::endl;
 	return (0);
 }
